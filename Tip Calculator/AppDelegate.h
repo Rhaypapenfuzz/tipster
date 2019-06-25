01c240e8f5828a2479215d542f20ce8dc2bcc332
Raymond Gyabeng <rhaypapenfuzz@fb.com>
2019-06-25 14:16:42 -0700

    Initial Commit

---
+++ Tip Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tip Calculator
+//
+//  Created by rhaypapenfuzz on 6/25/19.
+//  Copyright © 2019 rhaypapenfuzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tip Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip Calculator
+//
+//  Created by rhaypapenfuzz on 6/25/19.
+//  Copyright © 2019 rhaypapenfuzz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
